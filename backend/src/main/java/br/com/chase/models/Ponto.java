630faeca4f20a460b85441ab1f4e532db3f2af33
João Giatti <joaogiatti2006@gmail.com>
2025-10-13 18:43:55 -0300

    feat: Ponto class with getters and setters

---
+++ backend/src/main/java/br/com/chase/models/Ponto.java
@@ -0,0 +1,19 @@
+package br.com.chase.models;
+
+public class Ponto {
+    private double latitude;
+    private double longitude;
+
+    public Ponto() {}
+
+    public Ponto(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() { return latitude; }
+    public void setLatitude(double latitude) { this.latitude = latitude; }
+
+    public double getLongitude() { return longitude; }
+    public void setLongitude(double longitude) { this.longitude = longitude; }
+}
