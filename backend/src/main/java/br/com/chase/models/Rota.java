0f4ae734084e1e3de475c1b2c7e8dd4bb01784d8
João Giatti <joaogiatti2006@gmail.com>
2025-10-13 18:55:20 -0300

    feat: Rota service and controller for route creation

--- backend/src/main/java/br/com/chase/models/Rota.java
+++ backend/src/main/java/br/com/chase/models/Rota.java
@@ -19,5 +19,85 @@ public class Rota {
     private String visibilidade;
     private Date dataCriacao;
 
-    // getters e Setters omitidos pra deixar mais limpo
+    // 🔹 Getters e Setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCriadorId() {
+        return criadorId;
+    }
+
+    public void setCriadorId(String criadorId) {
+        this.criadorId = criadorId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Ponto> getPontos() {
+        return pontos;
+    }
+
+    public void setPontos(List<Ponto> pontos) {
+        this.pontos = pontos;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+
+    public String getTempoRecorde() {
+        return tempoRecorde;
+    }
+
+    public void setTempoRecorde(String tempoRecorde) {
+        this.tempoRecorde = tempoRecorde;
+    }
+
+    public double getVelocidadeMediaRecorde() {
+        return velocidadeMediaRecorde;
+    }
+
+    public void setVelocidadeMediaRecorde(double velocidadeMediaRecorde) {
+        this.velocidadeMediaRecorde = velocidadeMediaRecorde;
+    }
+
+    public double getCaloriasEstimadas() {
+        return caloriasEstimadas;
+    }
+
+    public void setCaloriasEstimadas(double caloriasEstimadas) {
+        this.caloriasEstimadas = caloriasEstimadas;
+    }
+
+    public String getVisibilidade() {
+        return visibilidade;
+    }
+
+    public void setVisibilidade(String visibilidade) {
+        this.visibilidade = visibilidade;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
 }
