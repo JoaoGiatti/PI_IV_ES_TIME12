02d390c891df6ebc4e6973c5847d794aca1b09d7
Jhenifer Laís Barbosa <jheniferlais3@gmail.com>
2025-11-03 08:41:26 -0300

    chore: initialize development environment #33
    - Initial project setup to start coding JUNIT
    - Added basic dependencies
    - Created folder structure for Anticheat server

---
+++ server/src/test/java/InterClasse/TestInterClasse.java
@@ -0,0 +1,14 @@
+package InterClasse;
+
+public class TestInterClasse {
+
+
+    // Normal: corrida válida → servidor processa e salva corretamente.
+
+    // Erro: corrida inválida / dados inconsistentes → servidor trata ou rejeita.
+
+    // Alternativo: corrida suspeita → servidor marca para revisão, envia alerta, ou faz fallback.
+
+
+
+}
---
+++ server/src/test/java/IntraClasse/TestIntraClasse.java
@@ -0,0 +1,17 @@
+package IntraClasse;
+
+public class TestIntraClasse {
+
+
+    // Atributo: velocidade média, distância percorrida, tempo total, desvios bruscos de rota.
+
+    // Estado: qualquer flag ou status incluído nos dados finais da corrida, como:
+        // precisão do GPS
+        // dados incompletos ou inconsistentes no polylines
+        // flags de sensor reportadas pelo cliente
+
+    // 4 classes
+
+
+
+}
