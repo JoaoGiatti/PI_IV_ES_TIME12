e21d1f1cc52e1f57c48ed53f8f990fda9fea100a
Thicosmo <thiago.vc2@puccampinas.edu.br>
2025-11-26 19:46:47 -0300

    feat: criar modelo inicial para histórico de tentativas (ainda não integrado)

---
+++ Data/Route server/src/main/java/com/httpserver/HttpServer.java
@@ -0,0 +1,14 @@
+package com.httpserver;
+
+/**
+ * Drive Class for the HTTP Server
+ */
+
+public class HttpServer {
+    public static void main(String[] args) {
+
+        System.out.println("Server Started");
+
+
+    }
+}
